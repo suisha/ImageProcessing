d6bef33de9a8a3daaf9320cff2b83cb4d2080123
David Mai <david.mai@isocket.com>
2015-11-09 23:46:24 -0800

    design rotate90

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var keys = ['r', 'g', 'b', 'a'];
 
 function processImage (img) {
   // img.height, .width, .data [r,g,b,a,r,g,b,a...]
   //copy(img)
   //transparent(img);
   //tintRed(img);
+  //foo(greyscale(img), greyscale2(img));
   //greyscale(img);
   //invertColors(img);
+  flipVertical(img);
+
   /*
   var pixels = genPixels(img);
   console.log(pixels[127]);
   img.data = renderPixels(pixels);
   */
 
-  greyscale(img);
+  //greyscale(img);
 
   /*
   Object.keys(img).forEach(function(i, v) {
@@ -24,12 +26,55 @@ function processImage (img) {
   */
 }
 
+/*
+12345
+67890
+abcde
+fghij
+klmno
+
+Go through the top left quadrant of the grid (width / 2, height / 2)
+
+For each pixel, perform a rotation:
+
+1. Get the x and y of the current pixel.
+newX = height - oldY
+newY = oldX
+*/
+function rotate90 (img) {
+}
+
+/*
+Go through every rgba in the first half, using rows.
+*/
+function flipVertical (img) {
+  for (var i = 0; i < (img.height / 2); i++) {
+    for (var ii = 1; ii <= img.width; ii++) {
+      for (var iii = 1; iii <= 4; iii++) {
+        var swapHeight = img.height - i;
+        var x = img.data[(i * img.width * 4) + (ii * 4) - iii];
+        var y = img.data[(swapHeight * img.width * 4) + (ii * 4) - iii];
+
+        img.data[(i * img.width * 4) + (ii * 4) - iii] = y;
+        img.data[(swapHeight * img.width * 4) + (ii * 4) - iii] = x;
+      }
+    }
+  }
+  return img;
+}
+
 // Using constants from http://stackoverflow.com/q/687261/886596
 function greyscale (img) {
   var f = function(pixel) {
-    pixel['r'] = 0.2989 * pixel['r'];
-    pixel['g'] = 0.5870 * pixel['g'];
+    var c = (0.2989 * pixel['r']) + (0.5870 * pixel['g']) + (0.114 * pixel['b']);
+
+    pixel['r'] = c;
+    pixel['g'] = c;
+    pixel['b'] = c;
+
   }
+
+  return mapPixels(img, f);
 }
 
 function invertColors (img) {
@@ -39,9 +84,7 @@ function invertColors (img) {
     pixel['b'] = 255 - pixel['b'];
   };
 
-  img.data = renderPixels(genPixels(img, f));
-
-  return img;
+  return mapPixels(img, f);
 }
 
 /*
@@ -52,11 +95,9 @@ Create an array of length 128. Whenever this array is filled, push it
 into a master array and create a new array.
 
 */
-function genPixels(img, f) {
-  var pixels = [];
+function mapPixels(img, f) {
   var foursome = [];
 
-  var row = [];
   for (var i = 0; i < img.data.length; i++) {
     foursome.push(img.data[i]);
     if (foursome.length === 4) {
@@ -67,37 +108,20 @@ function genPixels(img, f) {
         a: foursome[3],
       };
 
-
       if (f) {
         f(pixel);
       }
 
-      row.push(pixel);
+      img.data[i - 3] = pixel['r'];
+      img.data[i - 2] = pixel['g'];
+      img.data[i - 1] = pixel['b'];
+      img.data[i] = pixel['a'];
+
       foursome = [];
-      if (row.length === img.width) {
-        pixels.push(row);
-        row = [];
-      }
     }
   }
 
-  return pixels;
-}
-
-function renderPixels(pixels) {
-  var raw = [];
-
-  pixels.forEach(function(row) {
-    row.forEach(function(pixel) {
-
-      keys.forEach(function(k) {
-        raw.push(pixel[k]);
-      });
-
-    });
-  });
-
-  return raw;
+  return img;
 }
 
 function copy (img) {
@@ -139,19 +163,9 @@ function tintRed (img) {
 }
 
 
-
-
-
-function flipVertical (img) {
-  var news = [];
-
-  img.data = news;
-  return img;
-}
-
 function flipHorizontal (img) {}
 
-function rotate90 (img) {}
+
 
 function blur (img, blurFactor) {}
 
